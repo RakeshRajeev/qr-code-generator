0be7cb8b4c24827a82d195249a2ea0ad827ed008
RakeshRajeev <palanrakesh@gmail.com>
2025-03-13 15:57:41 +0530

    test error4

--- src/config.py
+++ src/config.py
@@ -6,10 +6,10 @@ from pydantic_settings import BaseSettings
 load_dotenv()
 
 class Settings(BaseSettings):
-    database_url: str = os.getenv("DATABASE_URL")
+    database_url: str = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost:5432/qr_code_db")
     redis_host: str = os.getenv("REDIS_HOST", "localhost")
     redis_port: int = int(os.getenv("REDIS_PORT", 6379))
-    celery_broker_url: str = os.getenv("CELERY_BROKER_URL")
+    celery_broker_url: str = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
 
     class Config:
         env_file = ".env"
